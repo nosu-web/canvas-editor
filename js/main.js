3049d45a47ce1cb1e9c5d40e44db2adfaf4bf422
NOSU-WEB <79894331+nosu-web@users.noreply.github.com>
2021-12-30 12:07:21 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
-function draw_text() {
-    var canvas = document.getElementById('text_canvas');
-    var ctx = canvas.getContext('2d')
-    var font = document.getElementById("select_font").value;
-    var text = document.getElementById('input_text').value
-    var colour1 = document.getElementById('input_col1').value
-    var colour2 = document.getElementById('input_col2').value
-    console.log(text, ' ', colour1, ' ', colour2, ' ', font)
-    
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+var canvas = document.getElementById('text_canvas');
 
-    var textWidth = ctx.measureText(text).width
+document.addEventListener("DOMContentLoaded", () => {
+    drawTshirt();
+});
+
+function drawTshirt() {
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var canvas_bg = "img/t-shirt.png";
+
+    var background = new Image();
+
+    background.onload = function(){
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+        drawText(ctx);
+    }
+    background.src = canvas_bg;
+}
+
+function drawText(ctx) {
+    var font = document.getElementById("select_font").value;
+    var text = document.getElementById('input_text').value;
+    var colour1 = document.getElementById('input_col1').value;
+    var colour2 = document.getElementById('input_col2').value;
+    
+    var textWidth = ctx.measureText(text).width;
     var gradient=ctx.createLinearGradient(0,0,textWidth,0);
     gradient.addColorStop("0",colour1);
     gradient.addColorStop("1",colour2);
 
     ctx.fillStyle=gradient;
     ctx.font = "30pt "+String(font);
-    ctx.fillText(text, 20, 50)
+    ctx.fillText(text, 20, 50);
 }
